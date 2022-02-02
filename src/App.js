a0c4c54b2584003c085a15ee97fd48bc652c80f1
Reinoptland <reinoptland@gmail.com>
2022-02-02 16:10:40 +0100

    Make ProductCard Reusable

--- src/App.js
+++ src/App.js
@@ -2,43 +2,29 @@ import "./App.css";
 import sedanIcon from "./images/icon-sedans.svg";
 import suvIcon from "./images/icon-suvs.svg";
 import luxuryIcon from "./images/icon-luxury.svg";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
-    <div class="container">
-      <div class="card card--orange">
-        <img src={sedanIcon} class="card__icon" />
-        <h2 class="card__title">Sedans</h2>
-        <p class="card__description">
-          Choose a sedan for its affordability and excellent fuel economy. Ideal
-          for cruising in the city or on your next road trip.
-        </p>
-        <button class="card__button-cta card__button-cta--orange">
-          Learn More
-        </button>
-      </div>
-      <div class="card card--cyan">
-        <img src={suvIcon} class="card__icon" />
-        <h2 class="card__title">SUVs</h2>
-        <p class="card__description">
-          Take an SUV for its spacious interior, power, and versatility. Perfect
-          for your next family vacation and off-road adventures.
-        </p>
-        <button class="card__button-cta card__button-cta--cyan">
-          Learn More
-        </button>
-      </div>
-      <div class="card card--dark-cyan">
-        <img src={luxuryIcon} class="card__icon" />
-        <h2 class="card__title">Luxury</h2>
-        <p class="card__description">
-          Cruise in the best car brands without the bloated prices. Enjoy the
-          enhanced comfort of a luxury rental and arrive in style.
-        </p>
-        <button class="card__button-cta card__button-cta--dark-cyan">
-          Learn More
-        </button>
-      </div>
+    <div className="container">
+      <ProductCard
+        title="Sedan"
+        description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        icon={sedanIcon}
+        color="orange"
+      />
+      <ProductCard
+        title="Suvs"
+        description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+        icon={suvIcon}
+        color="cyan"
+      />
+      <ProductCard
+        title="Luxury"
+        description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+        icon={luxuryIcon}
+        color="dark-cyan"
+      />
     </div>
   );
 }
---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ProductCard(props) {
+  //   console.log("PROPS:", props);
+  return (
+    <div className={`card card--${props.color}`}>
+      <img src={props.icon} className="card__icon" />
+      <h2 className="card__title">{props.title}</h2>
+      <p className="card__description">{props.description}</p>
+      <button className={`card__button-cta card__button-cta--${props.color}`}>
+        Learn More
+      </button>
+    </div>
+  );
+}
