ca4a96d1d74bd9d4620d354547c6b6ef2c3262cd
Reinoptland <reinoptland@gmail.com>
2022-02-02 15:03:37 +0100

    Migrate HTML & CSS to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sedanIcon from "./images/icon-sedans.svg";
+import suvIcon from "./images/icon-suvs.svg";
+import luxuryIcon from "./images/icon-luxury.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="container">
+      <div class="card card--orange">
+        <img src={sedanIcon} class="card__icon" />
+        <h2 class="card__title">Sedans</h2>
+        <p class="card__description">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button class="card__button-cta card__button-cta--orange">
+          Learn More
+        </button>
+      </div>
+      <div class="card card--cyan">
+        <img src={suvIcon} class="card__icon" />
+        <h2 class="card__title">SUVs</h2>
+        <p class="card__description">
+          Take an SUV for its spacious interior, power, and versatility. Perfect
+          for your next family vacation and off-road adventures.
+        </p>
+        <button class="card__button-cta card__button-cta--cyan">
+          Learn More
+        </button>
+      </div>
+      <div class="card card--dark-cyan">
+        <img src={luxuryIcon} class="card__icon" />
+        <h2 class="card__title">Luxury</h2>
+        <p class="card__description">
+          Cruise in the best car brands without the bloated prices. Enjoy the
+          enhanced comfort of a luxury rental and arrive in style.
+        </p>
+        <button class="card__button-cta card__button-cta--dark-cyan">
+          Learn More
+        </button>
+      </div>
     </div>
   );
 }
