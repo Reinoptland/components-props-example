f89c6f20da5f0543457ec54b01ea99e7363febf0
Reinoptland <reinoptland@gmail.com>
2022-02-03 08:35:12 +0100

    Add alt to icon img, delete logo

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,11 @@ import styles from "./ProductCard.module.css";
 export default function ProductCard(props) {
   return (
     <div className={`${styles["card"]} ${styles[`card--${props.color}`]} `}>
-      <img src={props.icon} className={styles["card__icon"]} />
+      <img
+        src={props.icon}
+        className={styles["card__icon"]}
+        alt={`Icon for ${props.title} cars`}
+      />
       <h2 className={styles["card__title"]}>{props.title}</h2>
       <p className={styles["card__description"]}>{props.description}</p>
       <button
